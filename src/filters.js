7ef538d476ce24a3fe689f1b0b95a68642b3a179
Pham Nguyen Truong Giang <giangpham.1751996@gmail.com>
2019-12-11 17:49:03 +0700

    Update filters.js

--- src/filters.js
+++ src/filters.js
@@ -1,14 +1,9 @@
 export default {
-  methods: {
-    coinFormat(value) {
-      let coin = new Intl.NumberFormat("vi-VI", {
-        style: "currency",
-        currency: "VND"
-      }).format(value);
-      return coin;
-    }
+  coinFormat(value) {
+    let coin = new Intl.NumberFormat("vi-VI", {
+      style: "currency",
+      currency: "VND"
+    }).format(value);
+    return coin;
   }
-  // coinFormat: function(value, length) {
-  //   return value.toFixed(length) + " đ";
-  // }
 };
